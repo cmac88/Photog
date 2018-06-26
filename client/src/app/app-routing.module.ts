5c1d579e2c212a98ce5cb7749fe0a7a91b274ae7
Samantha Withers <samantha.withers@cvshealth.com>
2018-06-26 15:49:52 -0400

    basic nav

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'gallery', component: GalleryComponent},
   {path: '', component: HomeComponent}
 ];
 
