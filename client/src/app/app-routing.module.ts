8ca7d6377b38227784bcd03066f8ee318b66542c
cmac88 <cmclaughlin88@gmail.com>
2018-06-26 15:23:06 -0700

    added gallery and packages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { LoginComponent } from './login/login.component';
+import { PackageComponent } from './package/package.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'portfolio', component: PortfolioComponent},
   {path: 'gallery', component: GalleryComponent},
+  {path: 'happy', component: PackageComponent},
   {path: '', component: HomeComponent}
 ];
 
