f219a8cbbffbcdecee911632c87a0fe51111e545
cmac88 <cmclaughlin88@gmail.com>
2018-06-26 13:37:08 -0700

    updated gallery login form

--- client/src/app/gallery/gallery.component.ts
+++ client/src/app/gallery/gallery.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpService } from '../http.service';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-
-  constructor() { }
+user: any;
+  constructor(private _http: HttpService) { }
 
   ngOnInit() {
   }
 
+  onSubmit(){
+
+  }
 }
