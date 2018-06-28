4e680c2a8b1e36ee7b291065b1111711c4a91047
Joshua_P <js.park189@gmail.com>
2018-06-28 14:17:21 -0700

    2nd commit, routing code updated to successfully route to shopping cart

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import {Routes, RouterModule} from '@angular/router';
+import { ProductComponent } from './+products/components/product/product.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'products'
-  },
-  {
-    path: 'products',
-    loadChildren: './+products/products.module#ProductsModule'
-  }
+// const moreRoutes: Routes[];
+
+export const moreRoutes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'products'},
+  {path: 'products', component: ProductComponent, loadChildren: './+products/products.module#ProductsModule'}
 ];
